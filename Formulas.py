da75fe72cfb1c3a0b09ed4762c12509abf6cf7a2
wang <ywang0789@conestogac.on.ca>
2024-12-10 03:01:08 -0500

    update formulas

--- Formulas.py
+++ Formulas.py
@@ -1,14 +1,30 @@
 import numpy as np
 
 
-def sigmoid(x: np.ndarray) -> np.ndarray:
+def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def sigmoid_derivative(x: np.ndarray) -> np.ndarray:
+def sigmoid_derivative(x):
     s = sigmoid(x)
     return s * (1 - s)
 
 
-def loss(y_true: np.ndarray, y_pred: np.ndarray) -> float:
-    return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+def loss(y_true, y_pred):
+    loss = -(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+    return loss
+
+
+def cost(y_true, y_pred, m):
+    cost = 1 / m * np.sum(loss(y_true, y_pred))
+    return cost
+
+
+def cost_w_derivative(X, y_true, y_pred, m):
+    dw = 1 / m * np.dot(X, (y_pred - y_true).T)
+    return dw
+
+
+def cost_b_derivative(y_true, y_pred, m):
+    db = 1 / m * np.sum(y_pred - y_true)
+    return db
