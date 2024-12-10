c57367f626b092bf8aab29ed7890acf00d59db2e
wang <ywang0789@conestogac.on.ca>
2024-12-10 08:07:09 -0500

    confidence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask, render_template, request
 
-from model.Model import load_model, predict
+from model.Model import load_model, predict, sigmoid
 
 app = Flask(__name__)
 
@@ -12,6 +12,7 @@ model_data, vectorizer = load_model("./model/model.pkl")
 @app.route("/", methods=["GET", "POST"])
 def index():
     prediction = None
+    confidence_level = None
     if request.method == "POST":
         email_text = request.form["email_text"]
 
@@ -20,10 +21,14 @@ def index():
         X = X_vec.toarray().T
 
         # Make prediction
-        Y_pred = predict(model_data["w"], model_data["b"], X)
+        A = sigmoid(np.dot(model_data["w"].T, X) + model_data["b"])
+        Y_pred = (A > 0.5).astype(int)
         prediction = "Phishing Email" if Y_pred[0][0] == 1 else "Safe Email"
+        confidence_level = round(A[0][0] * 100, 2)
 
-    return render_template("index.html", prediction=prediction)
+    return render_template(
+        "index.html", prediction=prediction, confidence_level=confidence_level
+    )
 
 
 if __name__ == "__main__":
